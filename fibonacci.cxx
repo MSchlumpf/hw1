87227c9cc05dff42317da95cb6e031db8a12506e
Michael Stumpf <mstumpf@pc03.pool.tp1.uni-duesseldorf.de>
2015-10-28 16:34:03 +0100

    hw done

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,6 +1,33 @@
+/*
+ * Calculating Fibonacci numbers
+ * author: Michael Stumpf (MSchlumpf)
+ */
+
 #include <iostream>
 
+using namespace std;
+
 int main(){
-  std::cout << "hallo" << std::endl;
+  int N = 1;
+  cout << "Bitte N eingeben: N = " << endl;
+  cin >> N;
+  int i,f,ff,fff;
+  fff = 0;
+  ff = 1;
+  if (N==0){
+    f=fff;
+    }
+  if (N==1){
+    f=ff;
+    }
+    
+ else
+  for(i=1;i<N;i++){
+  f = ff+fff;
+  fff = ff;
+  ff = f;
+  }
+  
+  cout << "Ihr Ergebnis lautet: f(" << N << ")=" << f << endl;
   return 0;
 }
